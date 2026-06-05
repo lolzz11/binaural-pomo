f74bb089be4d4f51dfad9811ed1465a0490ba6cc
lolzz11 <allforfree1@proton.me>
2026-06-06 00:26:58 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,40 @@
-const CACHE_NAME = 'binaural-flow-v56';
-self.addEventListener('install', (e) => self.skipWaiting());
-self.addEventListener('fetch', (e) => e.respondWith(fetch(e.request)));
+const CACHE_NAME = 'flowspace-v1';
+const ASSETS = [
+  './',
+  './index.html'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) return caches.delete(key);
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  if (!e.request.url.startsWith(self.location.origin)) return;
+
+  e.respondWith(
+    fetch(e.request)
+      .then((response) => {
+        const resClone = response.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(e.request, resClone));
+        return response;
+      })
+      .catch(() => caches.match(e.request))
+  );
+});
