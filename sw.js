6adf7eae029bf055c95e2f7a3a3e032f0631763e
lolzz11 <allforfree1@proton.me>
2026-06-06 13:22:37 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,40 +1,18 @@
 const CACHE_NAME = 'flowspace-v1';
 const ASSETS = [
-  './',
+  './', 
   './index.html'
 ];
 
 self.addEventListener('install', (e) => {
   e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(ASSETS);
-    })
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
   );
   self.skipWaiting();
 });
 
-self.addEventListener('activate', (e) => {
-  e.waitUntil(
-    caches.keys().then((keys) => {
-      return Promise.all(
-        keys.map((key) => {
-          if (key !== CACHE_NAME) return caches.delete(key);
-        })
-      );
-    })
-  );
-});
-
 self.addEventListener('fetch', (e) => {
-  if (!e.request.url.startsWith(self.location.origin)) return;
-
   e.respondWith(
-    fetch(e.request)
-      .then((response) => {
-        const resClone = response.clone();
-        caches.open(CACHE_NAME).then((cache) => cache.put(e.request, resClone));
-        return response;
-      })
-      .catch(() => caches.match(e.request))
+    fetch(e.request).catch(() => caches.match(e.request))
   );
 });
